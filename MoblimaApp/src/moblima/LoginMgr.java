dac00deca66e5a4f8c066a3cb879916959313fe0
quekyufei <quekyufei@hotmail.com>
2017-11-12 00:00:20 +0800

    edited loginmgr

--- MoblimaApp/src/moblima/LoginMgr.java
+++ MoblimaApp/src/moblima/LoginMgr.java
@@ -6,13 +6,31 @@ public class LoginMgr {
 	ArrayList<Account> staffArray;
 	ArrayList<Account> custArray;
 	
+	//return 0: the account.verifyLogin functions have failed
+	//return 1: login success
+	//return 2: wrong password
+	//return 3: user name not found in staff array
 	public int validate(int accType, String username, String password) {
-		int result;
+		int result = 0;
 		switch(accType) {
 		case 1:
 			for (Account account : staffArray) {
 				result = account.verifyLogin(username, password);
-					
+				if(result == 1) {
+					return result;
+				} else if(result == 2) {
+					return result;
+				}
+			}
+			return result;
+		case 2:
+			for (Account account : custArray) {
+				result = account.verifyLogin(username, password);
+				if(result == 1) {
+					return result;
+				} else if(result == 2) {
+					return result;
+				}
 			}
 		}
 	}
