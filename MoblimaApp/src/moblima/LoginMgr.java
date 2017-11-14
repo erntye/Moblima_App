85c0d1903d4637f2230c6bc3fd6ef57d140af6c8
Garion's Blade <yuxuanng@hotmail.com>
2017-11-14 22:30:52 +0800

    staff save

--- MoblimaApp/src/moblima/LoginMgr.java
+++ MoblimaApp/src/moblima/LoginMgr.java
@@ -105,8 +105,10 @@ public class LoginMgr {
 	public void addStaffAccount() {
 		String[] accountDetails = ConsoleBoundary.printAddStaffAccount();
 		StaffAcc newAcc = new StaffAcc(accountDetails[0],accountDetails[1],accountDetails[2]);
-		if(staffArray.add(newAcc))
+		if(staffArray.add(newAcc)) {
 			System.out.println("staff account added successfully!"); //for testing
+			DataBoundary.saveStaffList(staffArray);
+		}
 	}
 	
 	public void initialiseAccounts(ArrayList<Account> staffArray, ArrayList<Account> custArray) {
