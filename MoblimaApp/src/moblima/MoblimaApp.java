87e34d284bb88e5d523eb8b05b49330dc7f53746
Ernest <erntye@hotmail.com>
2017-11-14 22:21:43 +0800

    erw

--- MoblimaApp/src/moblima/MoblimaApp.java
+++ MoblimaApp/src/moblima/MoblimaApp.java
@@ -8,19 +8,7 @@ public class MoblimaApp {
 	public static void main(String[] args){
 		initialiseEverything();	//this is to read data from database
 		
-		//what does this chunk of this do?
 		String[] loginDetails = new String[3];
-		Calendar cal = Calendar.getInstance();
-		cal.set(2001,4,14);
-		Date date = cal.getTime();
-		Calendar cal2 = (Calendar) cal.clone();
-		cal2.add(Calendar.DATE,1);
-		Date date2 = cal.getTime();
-		System.out.println(cal.get(Calendar.MONTH));
-		System.out.println(date);
-		System.out.println(date2);
-		System.out.println(date.equals(date2));
-		System.out.println(cal.equals(cal2));
 		
 		//Main Menu: ask log in or create account
 		int mainMenuChoice;
