047403212a1eee7fd1bc7d3131aa1886b68bed70
Daniel Sebastian Yee <daniel-sebastian95@hotmail.com>
2017-11-16 23:02:52 +0800

    jd

--- MoblimaApp/src/moblima/PublicHolidayCalendar.java
+++ MoblimaApp/src/moblima/PublicHolidayCalendar.java
@@ -4,25 +4,46 @@ import java.util.ArrayList;
 import java.io.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-
+/**
+ * Entity class storing all the public holiday dates.
+ * <code>Show</code> dates are checked against this list to add a public holiday premium to its ticket prices.
+ * @author DanSeb
+ * @see Cinema
+ */
 public class PublicHolidayCalendar implements Serializable{					
-
+	/**
+	 * The premium to be added to ticket prices for shows screening on public holidays.  
+	 */
 	private static float publicHolidayPremium = 2.3f;
 	
 	private static final long serialVersionUID = -7989540302679891401L;
-	//Date class has to be created* using gregarion calendar to include pubHolList
-		
+	//Date class has to be created* using gregorion calendar to include pubHolList
+	/**
+	 * An <code>ArrayList</code> containing all the public holiday dates. 	
+	 */
 	public static ArrayList<Calendar> pubHolList = new ArrayList<Calendar>();
-		
+	
+	/**
+	 * @return <code>ArrayList&lt;Calendar&gt;</code> which contains the current list of public holidays.
+	 */
 	public ArrayList<Calendar> getPubHolList() {
 		return pubHolList;
 	}
 		
+	/**
+	 * Adds a <Calendar> date object to store the new public holiday date.
+	 * @param date
+	 */
 	public static void addPubHol(Calendar date) {
 		pubHolList.add(date);
 		DataBoundary.savePubHolList(pubHolList);
 	}
-		
+	
+	/**
+	 * Checks the <code> Show</code> date to check if it falls on a public holiday.
+	 * @param date
+	 * @return <code> Boolean</code> indicating whether the <code>Show</code> date falls on a public holiday.
+	 */
 	public static boolean isPubHol(Calendar date) {
 		for (int counter = 0; counter < pubHolList.size(); counter++) { 		      
 			if (pubHolList.get(counter).get(Calendar.MONTH) == date.get(Calendar.MONTH)){
@@ -33,10 +54,18 @@ public class PublicHolidayCalendar implements Serializable{
 		return false;	
 	}
 	
+	/**
+	 * Allows <code>StaffAcc</code> to change the price premium on public holiday tickets.
+	 * @param newPremium
+	 */
 	public static void setPublicHolidayPremium(float newPremium) {
 		publicHolidayPremium = newPremium;
 	}
-	
+
+	/**
+	 * 
+	 * @return <code> float</code> value of public holiday price premium.
+	 */
 	public static float getPublicHolidayPremium() {
 		return publicHolidayPremium;
 	}
--- MoblimaApp/src/moblima/Reviews.java
+++ MoblimaApp/src/moblima/Reviews.java
@@ -1,13 +1,26 @@
 package moblima;
 import java.io.*;
-
+/**
+ * Allows <code>CustAcc</code> to add <code>Reviews</code> for a <code>Movie</code> object.
+ * @see Movie
+ * @author DanSeb
+ */
 public class Reviews implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 8819093047305119458L;
+	/**
+	 * stores content <code>String</code> of <code>Reviews</code> object.
+	 */
 	private String content;
+	/**
+	 * stores rating <code>int</code> of <code>Reviews</code> object.
+	 */
 	private int rating;
+	/**
+	 * stores name of reviewer <code>String</code> of <code>Reviews</code> object.
+	 */
 	private String reviewer;
 	
 	public Reviews(String content, int rating, String reviewer){
@@ -15,15 +28,25 @@ public class Reviews implements Serializable{
 		this.rating = rating;
 		this.reviewer = reviewer;
 	}
-	
+	/**
+	 * 
+	 * @return <code>int</code> containing the rating of the review.
+	 */
 	public int getRating(){
 		return this.rating;
 	}
 	
+	/**
+	 * 
+	 * @return <code>String</code> containing the name of the reviewer.
+	 */
 	public String getReviewer(){
 		return this.reviewer;
 	}
-	
+	/**
+	 * 
+	 * @return <code>String</code> containing the content of the review.
+	 */
 	public String getContent(){
 		return this.content;
 	}
